1ac997616bfe29c86b244489602ac7955a954cf9
Samuel Tellez <tellezsamuel2102@gmail.com>
2026-07-20 20:08:25 -0500

    Usuarios recuperar contraseña

--- django/usuarios/urls.py
+++ django/usuarios/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),  # Solo login
+    path('login/', views.login_view, name='login'),
+    path('recuperar-contrasena/', views.recuperar_contrasena, name='recuperar_contrasena'),
+    path(
+    "cambiar-contrasena/",
+    views.cambiar_contrasena,
+    name="cambiar_contrasena",
+),
 ]
--- django/usuarios/views.py
+++ django/usuarios/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from barberos.models import Barbero
+from django.contrib.auth.hashers import make_password
+from clientes.models import Cliente
 
 def login_view(request):
     if request.method == 'POST':
@@ -47,3 +49,65 @@ def logout_view(request):
 
 def registro_view(request):
     return redirect('registro')
+
+def recuperar_contrasena(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        cedula = request.POST.get("cedula")
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            messages.error(request, "No existe una cuenta con ese correo.")
+            return render(request, "recuperar_contrasena.html")
+
+        cliente = Cliente.objects.filter(
+            user=user,
+            cedula=cedula
+        ).first()
+
+        barbero = Barbero.objects.filter(
+            email=email,
+            cedula=cedula,
+            activo=True
+        ).first()
+
+        if not cliente and not barbero:
+            messages.error(request, "Los datos no coinciden.")
+            return render(request, "recuperar_contrasena.html")
+
+        request.session["recuperar_usuario"] = user.id
+
+        return redirect("cambiar_contrasena")
+
+    return render(request, "recuperar_contrasena.html")
+
+
+def cambiar_contrasena(request):
+    usuario_id = request.session.get("recuperar_usuario")
+
+    if not usuario_id:
+        return redirect("recuperar_contrasena")
+
+    if request.method == "POST":
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        if password != confirm_password:
+            messages.error(request, "Las contraseñas no coinciden.")
+            return render(request, "cambiar_contrasena.html")
+
+        if len(password) < 6:
+            messages.error(request, "La contraseña debe tener al menos 6 caracteres.")
+            return render(request, "cambiar_contrasena.html")
+
+        user = User.objects.get(id=usuario_id)
+        user.set_password(password)
+        user.save()
+
+        del request.session["recuperar_usuario"]
+
+        messages.success(request, "Contraseña actualizada correctamente.")
+        return redirect("login")
+
+    return render(request, "cambiar_contrasena.html")
